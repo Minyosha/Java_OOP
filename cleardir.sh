7c38e472081af5af93c7ffbc4124561282d52e42
Minyosha <93713454+Minyosha@users.noreply.github.com>
2023-05-23 17:23:40 +0400

    Add files via upload

---
+++ cleardir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Введите путь к дирректории: " DELDIR
+
+if [ -e $DELDIR ]
+        then
+                echo 'Удаление файлов *.bak *.tmp *.backup из указанной директории'
+                cd $DELDIR                
+                rm -v *.bak *.tmp *.backup
+                echo 'Файлы *.bak *.tmp *.backup успешно удалены'
+        else
+                echo 'Указанная дирректории не существует'
+                exit 2
+fi
